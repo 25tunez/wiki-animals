8d696bbe443c91df6254d9879b69f7ce6e8d0ef0
25tunez <faithgrice@att.net>
2019-07-22 17:55:21 -0500

    first commit

---
+++ tkApp.py
@@ -0,0 +1,16 @@
+#create a "framework" to build tkinter interface
+
+import tkinter as tk
+
+class Application:
+    def __init__(self, size = "400x300", position = "+450+200"):
+        #main window, 
+        #window size, window position
+        
+def test_size_input:
+    #testing the size input
+    pass
+
+def test_position_input:
+    #testing the position input
+    pass
