8e1c271fd02c78b6f1bbda3c60156f9234c0d505
25tunez <faithgrice@att.net>
2019-07-22 21:20:17 -0500

    in animal script file, made functions out of everything, then imported that into the tkapp file and printed the results in tkApp.py

--- animalScript.py
+++ animalScript.py
@@ -4,20 +4,24 @@ import urllib.request as ur
 #scrape a wikipedia page for a list of animals
 
 #parse the wiki page
-urlToScrape = "https://en.wikipedia.org/wiki/List_of_animal_names"
-r = ur.urlopen(urlToScrape).read()
-soup = BeautifulSoup(r, "lxml") #transform URL to HTML that can be parsed
+def getSoup():
+    urlToScrape = "https://en.wikipedia.org/wiki/List_of_animal_names"
+    r = ur.urlopen(urlToScrape).read()
+    soup = BeautifulSoup(r, "lxml") #transform URL to HTML that can be parsed
+    return soup
 
 #extra step to make data more readable
-soup = soup.prettify() 
-soup = BeautifulSoup(soup, 'lxml')
+def enhanceSoup(soup):
+    soup = soup.prettify() 
+    soup = BeautifulSoup(soup, 'lxml')
+    return soup
 
-table_rows = soup.find_all("tr") #get all the table row tags
-animal_names = [] #to store the animal names
+def getAnimals(soup):
+    table_rows = soup.find_all("tr") #get all the table row tags
+    animal_names = [] #to store the animal names
 
-#info from first chart on wikipedia page
-for table_row in table_rows[2:13]: ##make this more general
-    table_data = table_row.contents[1] #second element of tr tag is a td
-    animal_names.append(table_data.a["title"]) #find a tag under table data and use the title attribute to store as the animal name
-
-print(animal_names)
+    #info from first chart on wikipedia page
+    for table_row in table_rows[2:13]: ##make this more general
+        table_data = table_row.contents[1] #second element of tr tag is a td
+        animal_names.append(table_data.a["title"]) #find a tag under table data and use the title attribute to store as the animal name
+    return animal_names
--- tkApp.py
+++ tkApp.py
@@ -1,17 +1,16 @@
 #create a "framework" to build tkinter interface
-
+import animalScript as aS
 import tkinter as tk
-
-class Application:
-    def __init__(self, master = tk.Tk(), size = "400x300", position = "+450+200", title = "New Window"):
-        #create the application
         
-
-        
-def test_size_input:
+def test_size_input():
     #testing the size input
     pass
 
-def test_position_input:
+def test_position_input():
     #testing the position input
     pass
+
+soup = aS.getSoup()
+soup = aS.enhanceSoup(soup)
+animals = aS.getAnimals(soup)
+print(animals)
