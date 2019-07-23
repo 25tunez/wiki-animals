dafdf83654af83cf2a024a153a7cf84795539c27
25tunez <faithgrice@att.net>
2019-07-22 20:05:22 -0500

    erased the self. lines in the application class

--- tkApp.py
+++ tkApp.py
@@ -3,9 +3,10 @@
 import tkinter as tk
 
 class Application:
-    def __init__(self, size = "400x300", position = "+450+200"):
-        #main window, 
-        #window size, window position
+    def __init__(self, master = tk.Tk(), size = "400x300", position = "+450+200", title = "New Window"):
+        #create the application
+        
+
         
 def test_size_input:
     #testing the size input
